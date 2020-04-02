61c4fef639a60771b9e91589100b1461e404efe1
Spencer W. Jensen <spencer.jensen@Spencers-MacBook-Pro-5.local>
2020-04-02 14:11:32 -0600

    test

--- djangofiles/arcticapi/settings.py
+++ djangofiles/arcticapi/settings.py
@@ -76,6 +76,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'arcticapi.wsgi.application'
 
+SECURE_BROWSER_XSS_FILTER = True
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
